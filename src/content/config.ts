22f2128c393ef4407fd93fd57f7dc087b3a04a3c
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2024-03-27 20:59:08 +0100

    Added the basic config for the content collections.

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,28 @@
-import { defineCollection, z } from "astro:content";
-
-const postCollection = defineCollection({
-    schema: z.object({
-        author: z.string(),
-        date: z.date(),
-        image: z.string(),
-        title: z.string(),
-    })
-})
+import { defineCollection, reference, z } from "astro:content";
 
 export const collections = {
-    posts: postCollection
-}
+  posts: defineCollection({
+    type: "content",
+    schema: z.object({
+      author: reference("authors"),
+      date: z.date(),
+      image: z.string(),
+      title: z.string().max(60, {
+        message: "Title must be 60 characters or less."
+      }),
+      description: z.string().max(200, {
+        message: "The description must be 200 characters or less."
+      }),
+      relatedPosts: z.array(reference("posts")).optional()
+    }),
+  }),
+  authors: defineCollection({
+    type: "data",
+    schema: z.object({
+        name: z.string(),
+        role: z.enum(["ADMIN", "CREATOR", "USER"]),
+        bio: z.string().max(400),
+        email: z.string().email()
+    })
+  })
+};
---
+++ src/content/posts/post-1.md
@@ -0,0 +1,10 @@
+---
+author: Alex-Sierro
+date: 2024-03-27
+image: "/src/assets/MainCarousel/Andrew Kliatskyi 3D Render.jpg"
+title: Title 1
+description: First test post
+---
+# This would be the first post
+
+This post is only for testing purposes.
